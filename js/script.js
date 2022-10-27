68290b05d721603bfb9a843ed1ee28e6e734d45b
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-10-26 19:32:43 -0700

    add message to addition, subtraction, multiply, and divide arguments.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,23 @@ function add(num1, num2) {
   return num1 + num2;
 }
 
+function subtract(num1 , num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+
 // user interface logic
-const num1 = prompt("Enter a number:");
-const num2 = prompt("Enter another number:");
-
-window.alert(add(num1, num2));
-
+const num1 = parseInt(prompt("Enter a number:"));
+const num2 = parseInt(prompt("Enter another number:"));
+window.alert("The sum of the addition problem is " + add(num1, num2) + ".");
+window.alert(`The remainder of your numbers is ${subtract(num1, num2)}.`);
+window.alert(`The product of your numbers is ${multiply(num1, num2)}.`);
+window.alert(`The quotient of your numbers is ${divide(num1, num2)}.`);

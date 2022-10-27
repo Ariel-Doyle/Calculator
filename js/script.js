874a10e795a245de192c127d2567f6122a235d1d
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-10-26 19:51:46 -0700

    add single window alert displaying inputted numbers in addition, subtraction, multiplication, and division equations.

--- js/script.js
+++ js/script.js
@@ -19,7 +19,13 @@ function divide(num1, num2) {
 // user interface logic
 const num1 = parseInt(prompt("Enter a number:"));
 const num2 = parseInt(prompt("Enter another number:"));
-window.alert("The sum of the addition problem is " + add(num1, num2) + ".");
-window.alert(`The remainder of your numbers is ${subtract(num1, num2)}.`);
-window.alert(`The product of your numbers is ${multiply(num1, num2)}.`);
-window.alert(`The quotient of your numbers is ${divide(num1, num2)}.`);
+
+/*window.alert(`The sum of the addition problem is ${add(num1, num2)}.
+The remainder of your numbers is ${subtract(num1, num2)}.
+The product of your numbers is ${multiply(num1, num2)}.
+The quotient of your numbers is ${divide(num1, num2)}.`);*/
+
+window.alert(`${num1} + ${num2} = ${add(num1, num2)}
+${num1} - ${num2} = ${subtract(num1, num2)}
+${num1} * ${num2} = ${multiply(num1 , num2)}
+${num1} / ${num2} = ${divide(num1, num2)}`);
